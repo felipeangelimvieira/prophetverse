bf172e417e372a1dd6cedf5fa41d6bdf5fc1900c
Franz Király <f.kiraly@ucl.ac.uk>
2024-05-01 18:58:30 +0100

    check_estimator

---
+++ tests/sktime/test_sktime_contract.py
@@ -0,0 +1,15 @@
+"""Test the sktime contract for Prophet and HierarchicalProphet."""
+
+import pytest
+from sktime.utils.estimator_checks import check_estimator
+
+from prophetverse.sktime import HierarchicalProphet, Prophet
+
+
+PROPHET_MODELS = [Prophet, HierarchicalProphet]
+
+
+@pytest.mark.parametrize("model", PROPHET_MODELS)
+def test_sktime_contract(model):
+    """Test the sktime contract for Prophet and HierarchicalProphet."""
+    check_estimator(model, raise_exceptions=True)

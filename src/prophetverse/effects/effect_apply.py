56ff37cdd2a3e651002f19b9ec58a61ada32be6e
Felipe Miranda <felipefm@id.uff.br>
2024-06-24 20:02:46 -0300

    refactor: modularize effects apply

---
+++ src/prophetverse/effects/effect_apply.py
@@ -0,0 +1,45 @@
+"""Defines the application of effects: additive or multiplicative."""
+
+import jax.numpy as jnp
+
+from prophetverse.utils.algebric_operations import matrix_multiplication
+
+
+def additive_effect(data: jnp.ndarray, coefficients: jnp.ndarray) -> jnp.ndarray:
+    """Apply an additive effect.
+
+    Parameters
+    ----------
+    data : jnp.ndarray
+        Data without the effect.
+    coefficients : jnp.ndarray
+        Effect vector with coefficients of each column.
+
+    Returns
+    -------
+    jnp.ndarray
+        Data with the effect applied.
+    """
+    return matrix_multiplication(data, coefficients)
+
+
+def multiplicative_effect(
+    trend: jnp.ndarray, data: jnp.ndarray, coefficients: jnp.ndarray
+) -> jnp.ndarray:
+    """Apply a multiplicative effect.
+
+    Parameters
+    ----------
+    trend : jnp.ndarray
+        Trend coefficient.
+    data : jnp.ndarray
+        Data without the effect.
+    coefficients : jnp.ndarray
+        Effect vector with coefficients of each column.
+
+    Returns
+    -------
+    jnp.ndarray
+        Data with the effect applied.
+    """
+    return trend * matrix_multiplication(data, coefficients)

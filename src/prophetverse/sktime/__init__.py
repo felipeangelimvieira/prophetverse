6326eae8bfb6bdac7d1c6883ea71da9bb35362c0
Felipe Angelim <felipeangelim@gmail.com>
2024-06-29 12:19:02 -0300

    Explicit imports in init

--- src/prophetverse/sktime/__init__.py
+++ src/prophetverse/sktime/__init__.py
@@ -1,2 +1,17 @@
+"""
+Implementation of the sktime API for the prophetverse library.
+
+This module provides a set of classes that implement the sktime API for the
+prophetverse library.
+"""
+
 from .multivariate import HierarchicalProphet
-from .univariate import *
+from .univariate import Prophet, ProphetGamma, ProphetNegBinomial, Prophetverse
+
+__all__ = [
+    "Prophet",
+    "ProphetGamma",
+    "ProphetNegBinomial",
+    "Prophetverse",
+    "HierarchicalProphet",
+]

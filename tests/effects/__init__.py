3d3fbf6069b0cb7504e011e6f3cc120e187f601b
Felipe Miranda <felipefm@id.uff.br>
2024-06-30 18:57:48 -0300

    fix: module import error

---
+++ tests/effects/__init__.py
@@ -0,0 +1,7 @@
+"""Public interface to effects module."""
+
+from prophetverse.effects.hill import HillEffect
+from prophetverse.effects.linear import LinearEffect
+from prophetverse.effects.log import LogEffect
+
+__all__ = ["HillEffect", "LinearEffect", "LogEffect"]

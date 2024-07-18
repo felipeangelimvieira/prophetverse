fdf0ab0691df0316c62df5df71e41600639c606a
Franz Király <f.kiraly@ucl.ac.uk>
2024-07-18 16:37:50 +0100

    Update test_sktime_check_estimator.py

--- tests/sktime/test_sktime_check_estimator.py
+++ tests/sktime/test_sktime_check_estimator.py
@@ -1,7 +1,7 @@
 """Test the sktime contract for Prophet and HierarchicalProphet."""
 
 import pytest
-from sktime.utils.estimator_checks import check_estimator
+from sktime.utils.estimator_checks import check_estimator, parametrize_with_checks
 
 from prophetverse.sktime import HierarchicalProphet, Prophetverse
 
@@ -10,10 +10,7 @@ PROPHET_MODELS = [
     HierarchicalProphet,
 ]
 
-
-@pytest.mark.skip(reason="Temporarily disabled")
-@pytest.mark.parametrize("model", PROPHET_MODELS)
-def test_check_estimator(model):
+@parametrize_with_checks(PROPHET_MODELS)
+def test_sktime_api_compliance(obj, test_name):
     """Test the sktime contract for Prophet and HierarchicalProphet."""
-
-    check_estimator(model, raise_exceptions=True)
+    check_estimator(obj, tests_to_run=test_name, raise_exceptions=True)

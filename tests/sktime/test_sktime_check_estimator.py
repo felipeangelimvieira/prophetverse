1592edc7addc3302ddb2d5c22fb6a4ed827b8c38
Felipe Angelim <felipeangelim@gmail.com>
2024-08-04 22:43:27 -0400

    Force gc collection

--- tests/sktime/test_sktime_check_estimator.py
+++ tests/sktime/test_sktime_check_estimator.py
@@ -1,5 +1,7 @@
 """Test the sktime contract for Prophet and HierarchicalProphet."""
 
+import gc
+
 import pytest  # noqa: F401
 from sktime.utils.estimator_checks import check_estimator, parametrize_with_checks
 
@@ -22,3 +24,4 @@ PROPHET_MODELS = [
 def test_sktime_api_compliance(obj, test_name):
     """Test the sktime contract for Prophet and HierarchicalProphet."""
     check_estimator(obj, tests_to_run=test_name, raise_exceptions=True)
+    gc.collect()

7df589cfb919eb4c082eecd51fab8f075aa79b91
Victor <victor.tingstrom@gmail.com>
2025-01-17 15:28:10 +0100

    Adds utility method and corresponding tests

---
+++ src/prophetverse/engine/utils.py
@@ -0,0 +1,41 @@
+from typing import Dict
+
+import jax.numpy as jnp
+import numpy as np
+
+from prophetverse.exc import ConvergenceError
+
+
+def assert_mcmc_converged(summary: Dict[str, Dict[str, jnp.ndarray]], max_r_hat: float):
+    """
+    Asserts that an MCMC program has converged.
+
+    Parameters
+    ----------
+    summary: Dict
+        MCMC trace summary.
+
+    max_r_hat: float
+        Maximum allowed r_hat.
+
+    Returns
+    -------
+        Nothing.
+
+    Raises
+    ------
+    ConvergenceError
+    """
+
+    for name, parameter_summary in summary.items():
+        # NB: some variables have deterministic elements (s.a. samples from LKJCov).
+        mask = np.isnan(parameter_summary["n_eff"])
+        r_hat = parameter_summary["r_hat"][~mask]
+
+        if (r_hat <= max_r_hat).all():
+            continue
+
+        # TODO: might be better to print entire summary instead of just which parameter didn't converge
+        raise ConvergenceError(f"Parameter '{name}' did not converge!")
+
+    return
---
+++ tests/engine/test_utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+import numpyro
+import pytest
+from jax.random import PRNGKey
+from numpyro.distributions import Normal, LKJCholesky
+from numpyro.diagnostics import summary
+from numpyro.infer import NUTS, MCMC
+
+from prophetverse.engine.utils import assert_mcmc_converged
+from prophetverse.exc import ConvergenceError
+
+
+@pytest.fixture
+def rng_key():
+    return PRNGKey(123)
+
+
+@pytest.fixture
+def shape():
+    return 4, 500
+
+
+@pytest.fixture
+def converged_summary(rng_key, shape):
+    samples = {
+        "alpha": Normal().sample(rng_key, shape),
+        "beta": LKJCholesky(4).sample(rng_key, shape),
+        "sigma": Normal().expand((2, 2)).to_event(2).sample(rng_key, shape)
+    }
+
+    return summary(samples, group_by_chain=len(shape) > 1)
+
+
+@pytest.fixture
+def not_converged_summary(rng_key, shape):
+    def model():
+        numpyro.sample("beta", LKJCholesky(4))
+        return
+
+
+    kernel = NUTS(model)
+    mcmc = MCMC(kernel, num_warmup=5, num_samples=5, num_chains=4)
+    mcmc.run(rng_key)
+
+    samples = mcmc.get_samples(group_by_chain=True)
+
+    return summary(samples, group_by_chain=True)
+
+
+def test_assert_converged(converged_summary):
+    assert_mcmc_converged(converged_summary, max_r_hat=1.1)
+
+
+def test_assert_error(not_converged_summary):
+    with pytest.raises(ConvergenceError):
+        assert_mcmc_converged(not_converged_summary, max_r_hat=1.1)

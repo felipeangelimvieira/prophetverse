ea937f3799c511ba7d9b1f5dddff61bcbae40a05
felipeangelimvieira <felipeangelim@gmail.com>
2025-05-14 23:11:14 -0300

    Fix lift likelihood and adstock integration

--- src/prophetverse/effects/adstock.py
+++ src/prophetverse/effects/adstock.py
@@ -6,7 +6,7 @@ import jax
 import jax.numpy as jnp
 import numpyro
 from numpyro import distributions as dist
-
+import pandas as pd
 from prophetverse.effects.base import BaseEffect
 
 __all__ = ["GeometricAdstockEffect"]
@@ -32,7 +32,7 @@ class GeometricAdstockEffect(BaseEffect):
     def __init__(
         self,
         decay_prior: dist.Distribution = None,
-        raise_error_if_fh_changes: bool = True,
+        raise_error_if_fh_changes: bool = False,
     ):
         self.decay_prior = decay_prior  # Default Beta distribution for decay rate.
         self.raise_error_if_fh_changes = raise_error_if_fh_changes
@@ -42,6 +42,21 @@ class GeometricAdstockEffect(BaseEffect):
         self._decay_prior = self.decay_prior
         if self._decay_prior is None:
             self._decay_prior = dist.Beta(2, 2)
+
+    def _fit(self, y, X, scale=1):
+        """Fit the effect to the data.
+
+        Parameters
+        ----------
+        y : jnp.ndarray
+            The target variable.
+        X : jnp.ndarray
+            The exogenous variables.
+        scale : float, optional
+            Scale factor for the effect (default is 1).
+        """
+        self._X = X.copy()
+        self._y = y.copy()
 
     def _transform(self, X, fh):
         """Transform the dataframe and horizon to array.
@@ -63,16 +78,25 @@ class GeometricAdstockEffect(BaseEffect):
         ValueError
             If the forecasting horizon is different during predict and fit.
         """
-        if self._min_date is None:
-            self._min_date = X.index.min()
-        else:
-            if self._min_date != X.index.min() and self.raise_error_if_fh_changes:
+
+        _X = X
+        dates_to_add = self._X.index.difference(X.index)
+        if len(dates_to_add) > 0:
+            if self.raise_error_if_fh_changes:
                 raise ValueError(
-                    "The X dataframe and forecat horizon"
+                    "The X dataframe and forecast horizon"
                     "must be start at the same"
                     "date as the previous one"
                 )
-        return super()._transform(X, fh)
+            _X = pd.concat([self._X.loc[dates_to_add, X.columns], X], axis=0)
+            _X = _X.sort_index()
+
+        # Get integer location of indexes of X in _X
+        ix = _X.index.get_indexer(X.index)
+        ix = jnp.array(ix, dtype=jnp.int32)
+
+        data = super()._transform(_X, fh)
+        return data, ix
 
     def _predict(
         self,
@@ -98,6 +122,7 @@ class GeometricAdstockEffect(BaseEffect):
         jnp.ndarray
             An array with shape (T, 1) for univariate timeseries.
         """
+        data, ix = data
 
         decay = numpyro.sample("decay", self._decay_prior)
 
@@ -108,6 +133,8 @@ class GeometricAdstockEffect(BaseEffect):
             return new_adstock, new_adstock
 
         _, adstock = jax.lax.scan(
-            adstock_step, init=jnp.array([0], dtype=data.dtype), xs=data
+            adstock_step, init=jnp.array([0], dtype=data.dtype), xs=data.flatten()
         )
-        return adstock.reshape(-1, 1)
+        adstock = adstock.reshape(-1, 1)
+        adstock = adstock[ix]
+        return adstock

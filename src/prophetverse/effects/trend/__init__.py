96fcb308b3ed281b13937d01ac6adc964dfd82be
Pablo <pcparedesp@hotmail.com>
2025-05-20 13:24:31 +0400

    Piecewise Flat Trend

--- src/prophetverse/effects/trend/__init__.py
+++ src/prophetverse/effects/trend/__init__.py
@@ -2,11 +2,12 @@
 
 from .base import TrendEffectMixin
 from .flat import FlatTrend
-from .piecewise import PiecewiseLinearTrend, PiecewiseLogisticTrend
+from .piecewise import PiecewiseLinearTrend, PiecewiseFlatTrend, PiecewiseLogisticTrend
 
 __all__ = [
     "TrendEffectMixin",
     "FlatTrend",
     "PiecewiseLinearTrend",
+    "PiecewiseFlatTrend",
     "PiecewiseLogisticTrend",
 ]

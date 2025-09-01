6c76049d149df8265b66405b81f7527ffcff5650
Victor <victor.tingstrom@gmail.com>
2025-09-01 12:02:09 +0200

    Typing

--- src/prophetverse/effects/target/univariate.py
+++ src/prophetverse/effects/target/univariate.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Callable
 
 import jax.numpy as jnp
 import pandas as pd
@@ -171,7 +171,7 @@ class NegativeBinomialTargetLikelihood(TargetLikelihood):
 
 def _build_positive_smooth_clipper(
     smooth_threshold: float, threshold: float = 1e-10
-) -> jnp.ndarray:
+) -> Callable[[jnp.ndarray], jnp.ndarray]:
     """Force the values of x to be positive.
 
     Applies a smooth threshold to the values of x to force positive-only outputs.
